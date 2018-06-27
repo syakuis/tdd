d9f93ade01fc0f59cae545f2b0826f2a21c76ced
최석균 <syaku@naver.com>
2018-06-27 10:43:36 +0900

    2. 타락한 객체

--- src/test/java/org/syaku/tdd/MoneyTest.java
+++ src/test/java/org/syaku/tdd/MoneyTest.java
@@ -5,15 +5,32 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
+ * close $5 * 2 = $10
+ * todo Dollar 부작용?
+ *
  * @author Seok Kyun. Choi. 최석균 (Syaku)
  * @since 2018. 6. 27.
  */
 public class MoneyTest {
-  @Test
-  public void testMultiplication() {
+
+  @Test(expected = AssertionError.class)
+  public void testMultiplicationError() {
     Dollar five = new Dollar(5);
     five.times(2);
     assertEquals(10, five.amount);
+    // five 5가 아니라 10인 상태
+    five.times(3);
+    // 그래서 5 * 3 이 아닌 10 * 3 이 되고 아래는 추정은 오류가 발생한다.
+    assertEquals(15, five.amount);
+  }
+
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount);
+    product = five.times(3);
+    assertEquals(15, product.amount);
   }
 
   class Dollar {
@@ -22,8 +39,8 @@ public class MoneyTest {
       this.amount = amount;
     }
 
-    void times(int multiplier) {
-      amount *= multiplier;
+    Dollar times(int multiplier) {
+      return new Dollar(amount * multiplier);
     }
   }
 }
